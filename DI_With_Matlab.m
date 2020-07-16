215b0f51776dded271fe213647cc578057338ddc
Dataq Instruments <45690253+dataq-instruments@users.noreply.github.com>
2020-07-16 15:17:31 -0400

    Add files via upload

---
+++ DI_With_Matlab.m
@@ -0,0 +1,77 @@
+
+% Dataq DI 1100 with MATLAB R2019b
+%
+% The DI 1100 must be in CDC mode
+%================================================
+
+clear, clc
+
+numOfData = 100;   
+% port = "/dev/tty.usbmodemFA131";    % For Mac OS
+port = "COM4";    % For Windows, in device manager's Ports (COM & LPT)
+baudrate = 115200;
+s = serialport(port, baudrate, "Timeout", 5);
+configureTerminator(s, "CR")
+% Clear serial buffer
+flush(s)
+
+% Check if device responds
+writeline(s, "info 1")
+pause(1)
+data = readline(s)
+
+% Configure
+
+% Stop in case device was already running
+writeline(s, "stop")
+pause(0.1)
+
+% Ending char for output in ascii mode is CR
+writeline(s, "eol 0")
+pause(0.1)
+
+% Set up for ascii communication
+writeline(s, "encode 1")
+pause(0.1)
+data = readline(s);
+
+% Set up channel(s) to scan
+writeline(s, "slist 0 0")
+pause(0.1)
+
+% Rate and other setup
+writeline(s, "srate 60000")
+pause(0.1)
+writeline(s, "dec 5")
+pause(0.1)
+writeline(s, "deca 1")
+pause(1)
+
+% Flush the port buffer
+flush(s)
+
+% Start scanning
+writeline(s, "start")
+pause(0.1)
+
+% Read data
+for i=1:numOfData
+    % Read the data
+    data(i) = readline(s);
+    % Convert to number
+    NumData(i) = str2double(data(i));
+end
+
+% Stop data acquisition
+writeline(s, "stop")
+
+% Plot data
+figure(1)
+plot(NumData, 'o-');
+grid on; ylabel('Value'); xlabel('sample')
+
+% Calculate average
+aveValue = mean(NumData)
+
+% Close the port
+clear s
---
+++ README.md
@@ -0,0 +1,6 @@
+# Matlab Example
+ Matlab example for DI-1100, 1110, 1120, 2108, 2108P, 4108, 4208, 4718 and 4730, USB mode only
+ To use it, one must change the device from LibUSB mode (WinDaq capable) to CDC mode first:
+ 
+ Connect the device to computer, once you see the green blinking, push and hold the button, until the LED turns white then red,
+ release the button and the device will be switched to CDC mode, and blinks yellow
